72015d6e98cd204c7b45aa59cfe01dd76dde6d52
Delphix Engineering <eng@delphix.com>
2023-07-15 03:13:05 +0000

    Version: 23.2.1-0ubuntu0~20.04.2
    -----BEGIN PGP SIGNED MESSAGE-----
    Hash: SHA512
    
    Format: 3.0 (quilt)
    Source: cloud-init
    Binary: cloud-init
    Architecture: all
    Version: 23.2.1-0ubuntu0~20.04.2
    Maintainer: Ubuntu Developers <ubuntu-devel-discuss@lists.ubuntu.com>
    Homepage: https://cloud-init.io/
    Standards-Version: 4.4.1
    Vcs-Browser: https://github.com/canonical/cloud-init/tree/ubuntu/devel
    Vcs-Git: https://github.com/canonical/cloud-init -b ubuntu/devel
    Build-Depends: debhelper (>= 9.20160709), dh-python, iproute2, po-debconf, python3, python3-configobj, python3-debconf, python3-jinja2, python3-jsonpatch, python3-jsonschema, python3-mock, python3-netifaces, python3-oauthlib, python3-pytest, python3-pytest-mock, python3-requests, python3-serial, python3-setuptools, python3-yaml, python3-responses
    Package-List:
     cloud-init deb admin optional arch=all
    Checksums-Sha1:
     8a3f9363d9a81fbc72432f2bdb43194fc51bc920 1551837 cloud-init_23.2.1.orig.tar.gz
     db5ddcb1a5bdfe71de32ac4d8f8bb01c25f5f9c1 90548 cloud-init_23.2.1-0ubuntu0~20.04.2.debian.tar.xz
    Checksums-Sha256:
     8f42c7bc5ef4857905c8e738c1b19319dbb1f2b0dc98e6792f1bd304d498a6af 1551837 cloud-init_23.2.1.orig.tar.gz
     82c4e5b3f05db49f68e652c708b3e0995cdb496b90bee1e7929ff6755c032fcd 90548 cloud-init_23.2.1-0ubuntu0~20.04.2.debian.tar.xz
    Files:
     a2edcab854b05b0cf834f35c9171a496 1551837 cloud-init_23.2.1.orig.tar.gz
     a1d83961839e1042dfb8443f6d1bda01 90548 cloud-init_23.2.1-0ubuntu0~20.04.2.debian.tar.xz
    Python-Version: all
    
    -----BEGIN PGP SIGNATURE-----
    
    iQIzBAEBCgAdFiEE4lRR4CIbV3Pev/F47NrLFgmVrYkFAmSdud8ACgkQ7NrLFgmV
    rYlOAhAAvATSApKLpS1lN3wM3Uv06K4gWc2kT77+uIhHAnQ/srZ2hY5Cv93zAYxF
    88ZdZtxOG9Fde8mT+bNJuD/VpMIvjFGI7brn3pKM23C8xiqzDfYTqBJWh9IUO2TK
    OoMTGYGYt7I6skwzgS+p65hrHg/y+p9rDQ2Em3XOXybpSgavwv4vc+ie26svD1Ob
    jkxT9JWt6CLpHPeaiTvB2ovL7oYqJaSR2QqlkfTXnDqu3IHsZqgs9731KcCF2Noh
    MLxwY9LGabb1VZFVCPp47iej2GQ1t04F/1N/34pFCzFryde6GnPJoE/2HhGgc+e7
    itL7OR3SGKmjuiRbuagmwmjZqsWEmQV001zRxuBWpG5uKhMoUP89ZiFsxs/XUO5c
    QuYhMPcvFPUShDdI8K68alaMo73wshOwZ1dCOFHNYH/12VtVuQbYbxqOXzLpxdC3
    YtgfOBQ/Pifzy1NzlDb0eO4236xpVeAwjynnVJ/e5CQRc0EG1cBJ22flR5K+vaJA
    A4EfTJc5kAeeshC/QOiw4DM/Mj2hhfyN4Qg4EuFLzdQ4LRhK4OLnrAkIZmWmIPzi
    2JxjT9XMR1x3oKlzREd2HHoZ3f+1tItxFjznfNNH3/hVx/k2tkIeW/J5aJQ8V/WM
    y+kBRLCN9qcZnNSjDQs1FoVgCRtRaYIpEAsPxP49j+GPre3uroQ=
    =IFe0
    -----END PGP SIGNATURE-----

---
+++ tests/integration_tests/test_kernel_commandline_match.py
@@ -0,0 +1,141 @@
+import logging
+
+import pytest
+
+from tests.integration_tests.clouds import IntegrationCloud
+from tests.integration_tests.conftest import get_validated_source
+from tests.integration_tests.instances import IntegrationInstance
+from tests.integration_tests.integration_settings import PLATFORM
+from tests.integration_tests.util import wait_for_cloud_init
+
+log = logging.getLogger("integration_testing")
+
+
+def override_kernel_cmdline(ds_str: str, c: IntegrationInstance) -> str:
+    """
+    Configure grub's kernel command line to tell cloud-init to use OpenStack
+    - even though LXD should naturally be detected.
+
+    This runs on LXD, but forces cloud-init to attempt to run OpenStack.
+    This will inevitably fail on LXD, but we only care that it tried - on
+    Ironic, for example, it will succeed.
+    """
+    client = c
+
+    # The final output in /etc/default/grub should be:
+    #
+    # GRUB_CMDLINE_LINUX="'ds=nocloud;s=http://my-url/'"
+    #
+    # That ensures that the kernel commandline passed into
+    # /boot/efi/EFI/ubuntu/grub.cfg will be properly single-quoted
+    #
+    # Example:
+    #
+    # linux /boot/vmlinuz-5.15.0-1030-kvm ro 'ds=nocloud;s=http://my-url/'
+    #
+    # Not doing this will result in a semicolon-delimited ds argument
+    # terminating the kernel arguments prematurely.
+    client.execute('printf "GRUB_CMDLINE_LINUX=\\"" >> /etc/default/grub')
+    client.execute('printf "\'" >> /etc/default/grub')
+    client.execute(f"printf '{ds_str}' >> /etc/default/grub")
+    client.execute('printf "\'\\"" >> /etc/default/grub')
+
+    # We should probably include non-systemd distros at some point. This should
+    # most likely be as simple as updating the output path for grub-mkconfig
+    client.execute("grub-mkconfig -o /boot/efi/EFI/ubuntu/grub.cfg")
+    client.execute("cloud-init clean --logs")
+    client.instance.shutdown(wait=False)
+    try:
+        client.instance.wait_for_state("STOPPED", num_retries=20)
+    except RuntimeError as e:
+        log.warning(
+            "Retrying shutdown due to timeout on initial shutdown request %s",
+            str(e),
+        )
+        client.instance.shutdown()
+
+    client.instance.execute_via_ssh = False
+    client.instance.start()
+    client.execute("cloud-init status --wait")
+    return client.execute("cat /var/log/cloud-init.log")
+
+
+@pytest.mark.skipif(PLATFORM != "lxd_vm", reason="Modifies grub config")
+@pytest.mark.lxd_use_exec
+@pytest.mark.parametrize(
+    "ds_str, configured",
+    (
+        (
+            "ds=nocloud;s=http://my-url/",
+            "DataSourceNoCloud [seed=None][dsmode=net]",
+        ),
+        ("ci.ds=openstack", "DataSourceOpenStack"),
+    ),
+)
+def test_lxd_datasource_kernel_override(
+    ds_str, configured, client: IntegrationInstance
+):
+    """This test is twofold: it tests kernel commandline override, which also
+    validates OpenStack Ironic requirements. OpenStack Ironic does not
+    advertise itself to cloud-init via any of the conventional methods: DMI,
+    etc.
+
+    On systemd, ds-identify is able to grok kernel commandline, however to
+    support cloud-init kernel command line parsing on non-systemd, parsing
+    kernel commandline in Python code is required.
+    """
+
+    assert (
+        "Machine is configured by the kernel commandline to run on single "
+        f"datasource {configured}"
+    ) in override_kernel_cmdline(ds_str, client)
+
+
+GH_REPO_PATH = "https://raw.githubusercontent.com/canonical/cloud-init/main/"
+
+
+@pytest.mark.skipif(PLATFORM != "lxd_vm", reason="Modifies grub config")
+@pytest.mark.lxd_use_exec
+@pytest.mark.parametrize(
+    "ds_str",
+    (f"ds=nocloud-net;s={GH_REPO_PATH}tests/data/kernel_cmdline_match/",),
+)
+def test_lxd_datasource_kernel_override_nocloud_net(
+    ds_str, session_cloud: IntegrationCloud
+):
+    """NoCloud requirements vary slightly from other datasources with parsing
+    nocloud-net due to historical reasons. Do to this variation, this is
+    implemented in NoCloud's ds_detect and therefore has a different log
+    message.
+    """
+    _ds_name, _, seed_url = ds_str.partition(";")
+    _key, _, url_val = seed_url.partition("=")
+    source = get_validated_source(session_cloud)
+    with session_cloud.launch(
+        launch_kwargs={
+            # On Jammy and above, we detect the LXD datasource using a
+            # socket available to the container. This prevents the socket
+            # from being exposed in the container, so LXD will not be detected.
+            # This allows us to wait for detection in 'init' stage with
+            # DataSourceNoCloudNet.
+            "config_dict": {"security.devlxd": False},
+            "wait": False,  # to prevent cloud-init status --wait
+        }
+    ) as client:
+        # We know this will be an LXD instance due to our pytest mark
+        client.instance.execute_via_ssh = False  # pyright: ignore
+        assert wait_for_cloud_init(client, num_retries=60).ok
+        if source.installs_new_version():
+            client.install_new_cloud_init(
+                source, take_snapshot=False, clean=False
+            )
+        logs = override_kernel_cmdline(ds_str, client)
+        assert (
+            "nocloud"
+            == client.execute("cloud-init query platform").stdout.strip()
+        )
+        assert url_val in client.execute("cloud-init query subplatform").stdout
+        assert (
+            "Detected platform: DataSourceNoCloudNet [seed=None]"
+            "[dsmode=net]. Checking for active instance data"
+        ) in logs
