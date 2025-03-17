e6adb6802ae9cb8b34d270b9bd7c0d19213a3d22
Delphix Engineering <eng@delphix.com>
2025-03-17 04:52:25 +0000

    Version: 24.4.1-0ubuntu0~24.04.1
    -----BEGIN PGP SIGNED MESSAGE-----
    Hash: SHA512
    
    Format: 3.0 (quilt)
    Source: cloud-init
    Binary: cloud-init
    Architecture: all
    Version: 24.4.1-0ubuntu0~24.04.1
    Maintainer: Ubuntu Developers <ubuntu-devel-discuss@lists.ubuntu.com>
    Homepage: https://cloud-init.io/
    Standards-Version: 4.5.0
    Vcs-Browser: https://github.com/canonical/cloud-init/tree/ubuntu/devel
    Vcs-Git: https://github.com/canonical/cloud-init -b ubuntu/devel
    Build-Depends: debhelper-compat (= 13), dh-python, iproute2, po-debconf, python3, python3-configobj, python3-debconf, python3-jinja2, python3-jsonpatch, python3-jsonschema, python3-mock, python3-oauthlib, python3-pytest, python3-pytest-mock, python3-requests, python3-serial, python3-setuptools, python3-yaml, python3-responses, python3-passlib
    Package-List:
     cloud-init deb admin optional arch=all
    Checksums-Sha1:
     a0aade6ef816a96a9698e9fa688585ea7d4612f2 1874945 cloud-init_24.4.1.orig.tar.gz
     a84fbe41f671c522d9a43e7786dfef105c6d2b80 99612 cloud-init_24.4.1-0ubuntu0~24.04.1.debian.tar.xz
    Checksums-Sha256:
     8d03cef01654be39dd712dba29d673b05c18f9ff40499d5bb20d4ec9d4af37c7 1874945 cloud-init_24.4.1.orig.tar.gz
     316496b17c7acc2c4c62de028cb19373f1c0715d3c7a223da6e79d1ebf47138b 99612 cloud-init_24.4.1-0ubuntu0~24.04.1.debian.tar.xz
    Files:
     48d37e4bdaa75c103a029d1c24d689d1 1874945 cloud-init_24.4.1.orig.tar.gz
     31db39f1dc44cc2bcec4ffa538d8e03c 99612 cloud-init_24.4.1-0ubuntu0~24.04.1.debian.tar.xz
    
    -----BEGIN PGP SIGNATURE-----
    
    iQGzBAEBCgAdFiEEsyC5UhjFbFFc1+BoZ0j1UGIrpdQFAmejnDkACgkQZ0j1UGIr
    pdSPIwv/WwYbu2c45g/19Ge0c4EyclQRwTG/dqQK2AEKs+LPYKZtB8/Ma9wjYJOa
    eLPtWhH+ikrjSYgY6xUEIiha/ZteyatHvyGCPLo/ZWrNkREXHp+zYQgpTHmuHIpF
    4YRton3pRul3P4mvOmy86TpkjfMYDqbnIiWoAFZzhpHiZ+eK/CNTJVM55tENHOMt
    Tovo+bZ/0LwAChv2GFlO0v3R0SkobzTyjIIvLxZ9yqLruWTA2SG7++EcaFs7pG7c
    BtWA9MU4hhNKjBRBpU2S4OSYTL6uPXB2iopTcxBY5mFnLEJJiaqLCwLkWKPGHdkx
    9JEXHOY6TjzguaNnCSTMeD/NzibHb1Uh0CF+iTYgRbkEt+inLSAAL25Ia9oR5Kfl
    j97iup2XEJexBMueicKJ+CnVyHl9nyE+dF04dqLHUHc3v8yCT8Qn2euWflI03Lrz
    3Czbg+N65waAxjxRZePDUZ9m5hMuLeBkyzIuVu0XJZaRMvNrSi4UHo592jsE/63s
    T00AHd+9
    =DxfB
    -----END PGP SIGNATURE-----

--- cloudinit/sources/__init__.py
+++ cloudinit/sources/__init__.py
@@ -17,7 +17,7 @@ import pickle
 import re
 from collections import namedtuple
 from enum import Enum, unique
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, List, NamedTuple, Optional, Tuple, Union
 
 from cloudinit import (
     atomic_helper,
@@ -193,6 +193,14 @@ DataSourceHostname = namedtuple(
 )
 
 
+class HotplugRetrySettings(NamedTuple):
+    """in seconds"""
+
+    force_retry: bool
+    sleep_period: int
+    sleep_total: int
+
+
 class DataSource(CloudInitPickleMixin, metaclass=abc.ABCMeta):
 
     dsmode = DSMODE_NETWORK
@@ -316,6 +324,14 @@ class DataSource(CloudInitPickleMixin, metaclass=abc.ABCMeta):
     # in the updated metadata
     skip_hotplug_detect = False
 
+    # AWS interface data propagates to the IMDS without a syncronization method
+    # Since no better alternative exists, use a datasource-specific mechanism
+    # which retries periodically for a set amount of time - apply configuration
+    # as needed. Do not force retry on other datasources.
+    #
+    # https://github.com/amazonlinux/amazon-ec2-net-utils/blob/601bc3513fa7b8a6ab46d9496b233b079e55f2e9/lib/lib.sh#L483
+    hotplug_retry_settings = HotplugRetrySettings(False, 0, 0)
+
     # Extra udev rules for cc_install_hotplug
     extra_hotplug_udev_rules: Optional[str] = None
 
@@ -360,6 +376,7 @@ class DataSource(CloudInitPickleMixin, metaclass=abc.ABCMeta):
             "skip_hotplug_detect": False,
             "vendordata2": None,
             "vendordata2_raw": None,
+            "hotplug_retry_settings": HotplugRetrySettings(False, 0, 0),
         }
         for key, value in expected_attrs.items():
             if not hasattr(self, key):
