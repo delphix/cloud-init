335af114e51f170fc345f149878f3eb460c5618e
Tony Perkins <tony.perkins@delphix.com>
2026-01-30 14:14:43 -0800

    Replace assertIn with assert x in y

--- tests/unittests/sources/test_vmware.py
+++ tests/unittests/sources/test_vmware.py
@@ -1042,10 +1042,7 @@ class TestDataSourceVMwareIMC:
                 ds.get_imc_data_fn,
             )
             assert result == (None, None, None)
-        assertIn(
-            "Error creating marker files",
-            self.logs.getvalue(),
-        )
+        assert "Error creating marker files" in self.logs.getvalue()
 
     def test_get_imc_data_cust_script_disabled(self, caplog, DS, tmpdir):
         """
@@ -1128,10 +1125,7 @@ class TestDataSourceVMwareIMC:
                     ds.get_imc_data_fn,
                 )
                 assert result == (None, None, None)
-        assertIn(
-            "Error creating marker files",
-            self.logs.getvalue(),
-        )
+        assert "Error creating marker files" in self.logs.getvalue()
 
     def test_get_imc_data_force_run_post_script_is_yes(
         self, caplog, DS, tmpdir
@@ -1180,10 +1174,7 @@ class TestDataSourceVMwareIMC:
                     ds.get_imc_data_fn,
                 )
                 assert result == (None, None, None)
-        assertIn(
-            "Error creating marker files",
-            self.logs.getvalue(),
-        )
+        assert "Error creating marker files" in self.logs.getvalue()
 
     def test_get_data_cloudinit_metadata_json(self, DS, tmpdir):
         """
